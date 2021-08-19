76e697efa91a542c68a215669be3dbe87164f193
Ömer Akça <omer.akca@signumtte.com>
2021-08-19 13:41:06 +0300

    test

--- jest.config.ts
+++ jest.config.ts
@@ -17,14 +17,14 @@ export default {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
+  // collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for
   // which coverage information should be collected
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: 'coverage',
+  // coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
   // coveragePathIgnorePatterns: [
@@ -106,7 +106,7 @@ export default {
 
   // Use this configuration option to add custom reporters to Jest
   // reporters: undefined,
-
+  reporters: ['default', 'jest-junit'],
   // Automatically reset mock state between every test
   // resetMocks: false,
 
